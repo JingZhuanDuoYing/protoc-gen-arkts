1264cb9f00799862a0c3c8bcffeaab3104cb0daf
donglua <foolfalcon@hotmail.com>
2026-04-16 11:19:31 +0800

    feat: add support for --version flag

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,14 @@ use protoc_gen_arkts::*;
 use crate::compile::compile;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        if args[1] == "--version" || args[1] == "-v" {
+            println!("protoc-gen-arkts {}", env!("CARGO_PKG_VERSION"));
+            return;
+        }
+    }
+
     let mut buffer: Vec<u8> = Vec::new();
     stdin()
         .read_to_end(&mut buffer)
